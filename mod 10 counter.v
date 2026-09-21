13588403282052c1b271a59e5227ae5ee43e8099
Kathir B <143430553+Kathir7788@users.noreply.github.com>
2026-09-21 15:17:26 +0530

    mod 10 counter.v

---
+++ mod 10 counter.v
@@ -0,0 +1,16 @@
+module mod10_counter(
+    input clk,
+    input rst,
+    output reg [3:0] count
+);
+
+always @(posedge clk) begin
+
+    if(rst)
+        count <= 0;
+    else if(count == 9)
+        count <= 0;
+    else
+        count <= count + 1;
+end
+endmodule
