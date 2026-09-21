99dcbe134391a30d267cc47624ba606a91f0389a
Kathir B <143430553+Kathir7788@users.noreply.github.com>
2026-09-21 14:09:42 +0530

    d_flipflop with asynchronous reset.v

---
+++ d_flipflop with asynchronous reset.v
@@ -0,0 +1,17 @@
+module dff_async(
+    input clk,
+    input rst,
+    input d,
+    output reg q
+);
+
+always @(posedge clk or posedge rst) begin
+
+    if(rst)
+        q <= 0;
+    else
+        q <= d;
+
+end
+
+endmodule
