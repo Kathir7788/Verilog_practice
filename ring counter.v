a31fa5ada283ee3099b2c389206272153b74cace
Kathir B <143430553+Kathir7788@users.noreply.github.com>
2026-09-21 16:25:00 +0530

    ring counter.v

---
+++ ring counter.v
@@ -0,0 +1,17 @@
+module ring_counter(
+    input clk,
+    input rst,
+    output reg [3:0] q
+);
+
+always @(posedge clk) begin
+
+    if(rst)
+        q <= 4'b0001;
+
+    else
+        q <= {q[2:0], q[3]};
+
+end
+
+endmodule
