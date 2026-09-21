0259fdab622b1f9ed85e85a6bd42d89978137798
Kathir B <143430553+Kathir7788@users.noreply.github.com>
2026-09-21 14:46:50 +0530

    SIPO register.v

---
+++ SIPO register.v
@@ -0,0 +1,18 @@
+module sipo(
+    input clk,
+    input rst,
+    input serial_in,
+    output reg [3:0] q
+);
+
+always @(posedge clk) begin
+
+    if(rst)
+        q <= 0;
+
+    else
+        q <= {q[2:0], serial_in};
+
+end
+
+endmodule
